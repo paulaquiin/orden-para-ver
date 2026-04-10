fa51c4e6e803d9259c808838740b0119e29490e1
Paula Quintana <paulaquintanagon00@gmail.com>
2026-04-10 11:57:02 +0200

    Feat: Nueva pantalla de descubrir

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         franchise: resolve(__dirname, 'franchise/index.html'),
         contenidos: resolve(__dirname, 'contenidos/index.html'),
+        descubrir: resolve(__dirname, 'descubrir/index.html'),
         novedades: resolve(__dirname, 'novedades/index.html'),
         privacidad: resolve(__dirname, 'privacidad/index.html'),
         terminos: resolve(__dirname, 'terminos/index.html'),
